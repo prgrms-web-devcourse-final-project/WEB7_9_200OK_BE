d751fc62d98d5c89a3f8466c57369795bd5b594e
myoungjinseo <sih5673@gmail.com>
2025-12-05 15:21:28 +0900

    Start setting

---
+++ src/main/java/com/windfall/WindfallApplication.java
@@ -0,0 +1,13 @@
+package com.windfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindfallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindfallApplication.class, args);
+    }
+
+}
