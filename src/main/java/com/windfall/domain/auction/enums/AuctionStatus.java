321ffefb15600205d6af90df8f66c6605393391a
YEAJI YUN <dpwl0974@gmail.com>
2025-12-09 16:33:16 +0900

    [Feature] 경매 상세 api swagger 이넘 추가 (#15)
    * feature: 경매 상세 api swagger 구현
    
    * feature: 경매 상세 api swagger 인터페이스 구현
    
    * feature: 경매 상세 응답 dto 생성
    
    * chore: 중복 제거
    
    * chore: 누락된 스키마 추가
    
    * chore: 누락 어노테이션 추가 및 주석 추가
    
    * chore: 불필요한 파일 ignore 설정
    
    * feature: 경매 상태 변경 이넘
    
    * feature: 판매자 감정표현 이모지 이넘
    
    * refactor: 생성된 이넘 적용
    
    * refactor: 감정표현 이모지 이넘 적용

--- src/main/java/com/windfall/domain/auction/enums/AuctionStatus.java
+++ src/main/java/com/windfall/domain/auction/enums/AuctionStatus.java
@@ -1,5 +1,17 @@
 package com.windfall.domain.auction.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum AuctionStatus {
-  END, START
+
+  SCHEDULED("경매 예정"),
+  PROCESS("경매 진행 중"),
+  COMPLETED("낙찰 완료"),
+  FAILED("유찰"),
+  CANCELED("경매 취소");
+
+  private final String description;
 }
