38ae9a0c82f28159dc28d0258276f4c47789a3df
Hodakrer <jihyuk0609@naver.com>
2025-12-09 14:34:18 +0900

    feature: User 도메인 관련 세팅 - 유저 엔티티 설정 - 유저 컨트롤러 설정 - 유저 Swagger용 userSpecification.java 설정 - 유저 provider type을 enum 클래스로 분리 - gradle에서 security 관련 주석화 - SwaggerConfig.java 오타 처리

---
+++ src/main/java/com/windfall/api/user/controller/UserSpecification.java
@@ -0,0 +1,9 @@
+//
+// 구체적으로 필요한 함수와 Swagger는 다음 PR 때 적겠습니다.
+package com.windfall.api.user.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "User", description = "사용자 API")
+public interface UserSpecification {
+}
--- src/main/java/com/windfall/domain/user/entity/User.java
+++ src/main/java/com/windfall/domain/user/entity/User.java
@@ -1,10 +1,44 @@
+//
+// 팩토리패턴 사용한 생성자 함수는 DTO 짜고 추가하겠습니다.
+// 다른 엔티티와 연관관계 매핑이 필요한 필드는 다음 PR 때 진행하겠습니다.
+// 사유: 다른 엔티티도 push하면 PR에서 검토할 파일 수가 증가하기 때문입니다.
 package com.windfall.domain.user.entity;
 
+import com.windfall.domain.user.enums.ProviderType;
 import com.windfall.global.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User extends BaseEntity {
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ProviderType provider;
+
+  @Column(nullable = false, name = "provider_user_id")
+  private String providerUserId;
+
+  @Column(nullable = false)
+  private String email;
+
+  // 일대일, 일대다 연관관계 매핑은 다음 pull request 때 진행하겠습니다.
+
+  // 필드 추가 시 여기도 손봐주세요.
+  @Builder
+  public User(ProviderType provider, String provideruserId, String email) {
+    this.provider = provider;
+    this.providerUserId = provideruserId;
+    this. email = email;
+  }
 }
