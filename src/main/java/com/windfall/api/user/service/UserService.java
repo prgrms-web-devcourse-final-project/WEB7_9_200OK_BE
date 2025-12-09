d9beeae63ead88fd9b9bf8355674954e664ddafe
myoungjinseo <sih5673@gmail.com>
2025-12-09 17:00:43 +0900

    feat: getUserById #8

--- src/main/java/com/windfall/api/user/service/UserService.java
+++ src/main/java/com/windfall/api/user/service/UserService.java
@@ -1,7 +1,20 @@
 package com.windfall.api.user.service;
 
+import com.windfall.domain.user.entity.User;
+import com.windfall.domain.user.repository.UserRepository;
+import com.windfall.global.exception.ErrorCode;
+import com.windfall.global.exception.ErrorException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+  private final UserRepository userRepository;
+
+  public User getUserById(Long userId) {
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new ErrorException(ErrorCode.NOT_FOUND_USER));
+  }
 }
--- src/main/java/com/windfall/global/exception/ErrorCode.java
+++ src/main/java/com/windfall/global/exception/ErrorCode.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 에러")
-
-    ;
-    private final HttpStatus httpStatus;
-    private final String message;
+  NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "사용자를 찾을 수 없습니다."),
+  UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 에러")
+  ;
+  private final HttpStatus httpStatus;
+  private final String message;
 }
