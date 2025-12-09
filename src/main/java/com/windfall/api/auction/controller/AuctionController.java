9297941030fd1f1c1b54c33edb38abad9583b153
myoungjinseo <sih5673@gmail.com>
2025-12-09 18:46:22 +0900

    refactor: 스웨거 #8

--- src/main/java/com/windfall/api/auction/controller/AuctionController.java
+++ src/main/java/com/windfall/api/auction/controller/AuctionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/auctions")
+@RequestMapping("/api/v1/auctions")
 @RequiredArgsConstructor
 public class AuctionController implements AuctionSpecification {
 
--- src/main/java/com/windfall/api/auction/dto/request/AuctionCreateRequest.java
+++ src/main/java/com/windfall/api/auction/dto/request/AuctionCreateRequest.java
@@ -1,18 +1,44 @@
 package com.windfall.api.auction.dto.request;
 
 import com.windfall.domain.auction.enums.AuctionCategory;
-import com.windfall.domain.auction.enums.AuctionStatus;
-import java.time.LocalDateTime;
-import org.springframework.format.annotation.DateTimeFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import lombok.NonNull;
+
+
+@Schema(description = "경매 생성 응답 DTO")
 public record AuctionCreateRequest(
+    @NonNull
+    @Schema(description = "판매자 id", example = "1")
     Long sellerId,
+
+    @NonNull
+    @Schema(description = "경매 제목", example = "테스트 제목")
     String title,
+
+    @NonNull
+    @Schema(description = "경매 설명", example = "테스트 설명")
     String description,
+
+    @NonNull
+    @Schema(description = "경매 카테고리", example = "1")
     AuctionCategory category,
+
+    @NonNull
+    @Schema(description = "경매 시작가", example = "10000")
     Long startPrice,
+
+    @NonNull
+    @Schema(description = "경매 스탑 로스", example = "9000")
     Long stopLoss,
+
+    @NonNull
+    @Schema(description = "경매 하락 금액", example = "50")
     Long dropAmount,
+
+    @NonNull
+    @Schema(description = "경매 시작 시간", example = "2025-12-09 16:50:51:23")
     LocalDateTime startAt
 ) {
 }
