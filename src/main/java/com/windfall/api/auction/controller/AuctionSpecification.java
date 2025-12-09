b515cd838bcdc38de41b4e9ba69699c0e8b06da0
YEAJI YUN <dpwl0974@gmail.com>
2025-12-09 17:37:40 +0900

    [Feature] 경매 상세 api 예외 처리 swagger 구현 (#18)
    * feature: 경매 상세 조회 예외 swagger 추가
    
    * feature: 경매 가격 변동 내역 예외 swagger 추가
    
    * feature: 이모지 전송 예외 swagger 추가
    # Conflicts:
    #	src/main/java/com/windfall/api/auction/controller/AuctionSpecification.java
    #	src/main/java/com/windfall/global/exception/ErrorCode.java

--- src/main/java/com/windfall/api/auction/controller/AuctionSpecification.java
+++ src/main/java/com/windfall/api/auction/controller/AuctionSpecification.java
@@ -2,9 +2,15 @@ package com.windfall.api.auction.controller;
 
 import com.windfall.api.auction.dto.request.AuctionCreateRequest;
 import com.windfall.api.auction.dto.response.AuctionCreateResponse;
+import static com.windfall.global.exception.ErrorCode.AUCTION_NOT_PROCESS;
+import static com.windfall.global.exception.ErrorCode.INVALID_AUCTION_SELLER;
+import static com.windfall.global.exception.ErrorCode.NOT_FOUND_AUCTION;
+import static com.windfall.global.exception.ErrorCode.NOT_FOUND_MEMBER;
+
 import com.windfall.api.auction.dto.response.AuctionDetailResponse;
 import com.windfall.api.auction.dto.response.AuctionHistoryResponse;
 import com.windfall.domain.auction.enums.EmojiType;
+import com.windfall.global.config.swagger.ApiErrorCodes;
 import com.windfall.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "Auction", description = "경매 상세 및 상호작용 API")
 public interface AuctionSpecification {
 
+  @ApiErrorCodes({NOT_FOUND_MEMBER, NOT_FOUND_AUCTION})
   @Operation(summary = "경매 생성", description = "새로운 경매를 생성합니다.")
   ApiResponse<AuctionCreateResponse> createAuction(
       @RequestBody AuctionCreateRequest request
@@ -29,12 +36,14 @@ public interface AuctionSpecification {
       @RequestBody Long userId
   );
 
+  @ApiErrorCodes({NOT_FOUND_AUCTION})
   @Operation(summary = "경매 가격 변동 내역 조회", description = "특정 경매의 가격 변동 내역을 조회합니다.")
   ApiResponse<AuctionHistoryResponse> getAuctionHistory(
       @Parameter(description = "경매 ID", required = true, example = "1")
       @PathVariable Long auctionId
   );
 
+  @ApiErrorCodes({NOT_FOUND_AUCTION, NOT_FOUND_MEMBER, AUCTION_NOT_PROCESS ,INVALID_AUCTION_SELLER})
   @Operation(summary = "경매 이모지 전송", description = "특정 경매에 대해 판매자가 이모지를 전송합니다.")
   ApiResponse<Void> sendEmoji(
       @Parameter(description = "경매 ID", required = true, example = "1")
@@ -46,5 +55,4 @@ public interface AuctionSpecification {
       @Parameter(description = "사용자 ID", required = true, example = "42")
       @RequestBody Long userId
   );
-
 }
--- src/main/java/com/windfall/global/exception/ErrorCode.java
+++ src/main/java/com/windfall/global/exception/ErrorCode.java
@@ -8,9 +8,18 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+  // 유저
   NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
+  // 경매
+  NOT_FOUND_AUCTION(HttpStatus.NOT_FOUND, "존재하지 않는 경매입니다."),
+  AUCTION_NOT_PROCESS(HttpStatus.BAD_REQUEST, "경매가 진행 중인 상태가 아닙니다."),
+  INVALID_AUCTION_SELLER(HttpStatus.FORBIDDEN, "해당 경매의 판매자가 아닙니다."),
+
+  // 그 외
   UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 에러")
   ;
+
   private final HttpStatus httpStatus;
   private final String message;
 }
