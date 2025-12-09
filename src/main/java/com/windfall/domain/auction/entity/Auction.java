bc8865143759fadf7d53ecafb842efd47be6e781
myoungjinseo <sih5673@gmail.com>
2025-12-09 12:31:56 +0900

    style: 구글 코드 스타일 변경 #5

--- src/main/java/com/windfall/domain/auction/entity/Auction.java
+++ src/main/java/com/windfall/domain/auction/entity/Auction.java
@@ -1,7 +1,6 @@
 package com.windfall.domain.auction.entity;
 
 
-
 import com.windfall.domain.auction.enums.AuctionCategory;
 import com.windfall.domain.auction.enums.AuctionStatus;
 import com.windfall.domain.user.entity.User;
@@ -18,39 +17,39 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Auction extends BaseEntity {
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "seller_id", nullable = false)
-    private User seller;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "seller_id", nullable = false)
+  private User seller;
 
-    @Column(nullable = false)
-    private String title;
+  @Column(nullable = false)
+  private String title;
 
-    @Column(columnDefinition = "TEXT",nullable = false)
-    private String description;
+  @Column(columnDefinition = "TEXT", nullable = false)
+  private String description;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private AuctionCategory category;
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private AuctionCategory category;
 
-    @Column(nullable = false)
-    private Long startPrice;
+  @Column(nullable = false)
+  private Long startPrice;
 
-    @Column(nullable = false)
-    private Long currentPrice;
+  @Column(nullable = false)
+  private Long currentPrice;
 
-    @Column(nullable = false)
-    private Long stopLoss;
+  @Column(nullable = false)
+  private Long stopLoss;
 
-    @Column(nullable = false)
-    private Long dropAmount;
+  @Column(nullable = false)
+  private Long dropAmount;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private AuctionStatus status;
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private AuctionStatus status;
 
-    @Column(nullable = false)
-    private LocalDateTime startedAt;
+  @Column(nullable = false)
+  private LocalDateTime startedAt;
 
-    private LocalDateTime endedAt;
+  private LocalDateTime endedAt;
 
 }
