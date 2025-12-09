4dd242d19fd562e6c8c22ede956a4d4f239dbf1b
myoungjinseo <sih5673@gmail.com>
2025-12-09 18:47:20 +0900

    refactor: 상태 값 변경 #8

--- src/main/java/com/windfall/global/exception/ErrorCode.java
+++ src/main/java/com/windfall/global/exception/ErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-  NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "사용자를 찾을 수 없습니다."),
-  UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 에러")
+  NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+  UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 에러")
   ;
   private final HttpStatus httpStatus;
   private final String message;
