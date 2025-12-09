571adbb01f691e3910d749d78359f9504474c825
dpwl0974 <dpwl0974@gmail.com>
2025-12-09 16:13:10 +0900

    feature: 판매자 감정표현 이모지 이넘

---
+++ src/main/java/com/windfall/domain/auction/enums/EmojiType.java
@@ -0,0 +1,13 @@
+package com.windfall.domain.auction.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EmojiType {
+  LIKE,
+  FIRE,
+  SAD,
+  SMILE;
+}
