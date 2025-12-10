856bfe69dfb923416dfa99f408c5959870426cd9
dpwl0974 <dpwl0974@gmail.com>
2025-12-10 22:52:50 +0900

    feature: 경매 상세 단순 계산 조회 서비스 로직 구현

---
+++ src/main/java/com/windfall/domain/auction/repository/AuctionPriceHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.windfall.domain.auction.repository;
+
+import com.windfall.domain.auction.entity.AuctionPriceHistory;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuctionPriceHistoryRepository extends JpaRepository<AuctionPriceHistory, Long> {
+
+  List<AuctionPriceHistory> findTop5ByAuction_IdOrderByCreateDateDesc(Long auctionId);
+
+}
