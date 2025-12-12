e52c63d557791d16b1acb72fcc7182082886acfc
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-12-12 12:42:24 +0900

    feat: 경매 태그 매핑 엔티티 추가, Tag 엔티티 수정

---
+++ src/main/java/com/windfall/domain/tag/entity/AuctionTag.java
@@ -0,0 +1,27 @@
+package com.windfall.domain.tag.entity;
+
+import com.windfall.domain.auction.entity.Auction;
+import com.windfall.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuctionTag extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "auction_id")
+  Auction auction;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "tag_id")
+  Tag tag;
+}
--- src/main/java/com/windfall/domain/tag/entity/Tag.java
+++ src/main/java/com/windfall/domain/tag/entity/Tag.java
@@ -1,11 +1,8 @@
 package com.windfall.domain.tag.entity;
 
-import com.windfall.domain.auction.entity.Auction;
 import com.windfall.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,10 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Tag extends BaseEntity {
-
-  @ManyToOne
-  @JoinColumn(name = "auction_id", nullable = false)
-  private Auction auction;
 
   @Column(nullable = false)
   private String tagName;
