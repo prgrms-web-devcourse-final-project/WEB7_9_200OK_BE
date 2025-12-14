0d4224e28815ab21d4380fc0e7ec1b53482b92f1
Ho-JunYu <hojoon1011@gmail.com>
2025-12-15 00:26:41 +0900

    feat: ChatRoomService - getChatRooms 메서드 생성 및 유저 검증 로직 추가

--- src/main/java/com/windfall/api/chat/service/ChatRoomService.java
+++ src/main/java/com/windfall/api/chat/service/ChatRoomService.java
@@ -1,10 +1,24 @@
 package com.windfall.api.chat.service;
 
+import com.windfall.api.chat.dto.request.enums.ChatRoomScope;
+import com.windfall.api.chat.dto.response.ChatRoomListResponse;
+import com.windfall.api.user.service.UserService;
+import com.windfall.domain.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatRoomService {
+
+  private final UserService userService;
+
+  public List<ChatRoomListResponse> getChatRooms(Long userId, ChatRoomScope scope) {
+
+    User me = userService.getUserById(userId);
+  }
 
 }
