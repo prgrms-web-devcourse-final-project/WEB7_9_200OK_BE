d81b60f4b545431309941bfc6a19779f806ae727
YEAJI YUN <dpwl0974@gmail.com>
2025-12-15 11:47:44 +0900

    [Feature] 경매 상세 스케줄러 도입 (#37)
    * feature: 스케줄러 사용을 위한 어노테이션 추가
    
    * feature: 가격 변동을 위한 로직 추가
    
    * feature: 가격 및 상태 변경을 위한 조회 로직 추가
    
    * feature: 경매 상태 변경 및 가격 변동 service 로직 구현
    
    * refactor: redis 조회 로직 추가로 실시간 접속자 수 내역 저장
    
    * refactor: 원활한 로그 확인을 위한 추가
    
    * refactor: 일관성있도록 스케줄러 설정 변경
    
    * refactor: 데이터 정합성 및 데이터 필요한 경우 대비 히스토리 내역 저장
    
    * refactor: 확장성 고려 및 역할 분리를 위한 재구조화
    
    * refactor: null이 아닌 응답값 반환

--- src/main/java/com/windfall/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/windfall/domain/auction/repository/AuctionRepository.java
@@ -1,8 +1,14 @@
 package com.windfall.domain.auction.repository;
 
 import com.windfall.domain.auction.entity.Auction;
+import com.windfall.domain.auction.enums.AuctionStatus;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuctionRepository extends JpaRepository<Auction,Long> {
 
+  List<Auction> findAllByStatusAndStartedAtLessThanEqual(AuctionStatus status, LocalDateTime now);
+
+  List<Auction> findAllByStatus(AuctionStatus status);
 }
