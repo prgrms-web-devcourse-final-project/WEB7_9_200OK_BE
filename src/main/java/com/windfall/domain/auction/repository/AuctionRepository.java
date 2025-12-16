92eea6f23e21843da84bdd7091c9c8666eeb9da7
myoungjinseo <sih5673@gmail.com>
2025-12-16 10:53:20 +0900

    feat: 경매 다건 조회 #34

--- src/main/java/com/windfall/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/windfall/domain/auction/repository/AuctionRepository.java
@@ -3,6 +3,6 @@ package com.windfall.domain.auction.repository;
 import com.windfall.domain.auction.entity.Auction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionRepository extends JpaRepository<Auction,Long> {
+public interface AuctionRepository extends JpaRepository<Auction,Long>, AuctionRepositoryCustom {
 
 }
---
+++ src/main/java/com/windfall/domain/auction/repository/AuctionRepositoryCustomImpl.java
@@ -0,0 +1,107 @@
+package com.windfall.domain.auction.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.windfall.api.auction.dto.response.info.PopularInfo;
+import com.windfall.api.auction.dto.response.info.ProcessInfo;
+import com.windfall.api.auction.dto.response.info.ScheduledInfo;
+import com.windfall.domain.auction.enums.AuctionStatus;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+import static com.windfall.domain.auction.entity.QAuction.auction;
+import static com.windfall.domain.auction.entity.QAuctionImage.auctionImage;
+import static com.windfall.domain.auction.entity.QAuctionPriceHistory.auctionPriceHistory;
+
+@RequiredArgsConstructor
+public class AuctionRepositoryCustomImpl implements AuctionRepositoryCustom{
+
+  private final JPAQueryFactory queryFactory;
+
+  @Override
+  public List<ProcessInfo> getProcessInfo(AuctionStatus status, int limit){
+    return queryFactory
+            .select(Projections.constructor(ProcessInfo.class,
+                auction.id,
+                JPAExpressions
+                    .select(auctionImage.image)
+                    .from(auctionImage)
+                    .where(auctionImage.auction.id.eq(auction.id))
+                    .orderBy(auctionImage.id.asc())
+                    .limit(1),
+                auction.title,
+                auction.startPrice,
+                auction.currentPrice,
+                calculateDiscountRate(),
+                Expressions.constant(false),
+                auction.startedAt
+            ))
+            .from(auction)
+            .where(auction.status.eq(status))
+            .orderBy(auction.startedAt.asc())
+            .limit(limit)
+            .fetch();
+  }
+
+  @Override
+  public List<ScheduledInfo> getScheduledInfo(AuctionStatus status, int limit){
+    return queryFactory
+        .select(Projections.constructor(ScheduledInfo.class,
+            auction.id,
+            JPAExpressions
+                .select(auctionImage.image)
+                .from(auctionImage)
+                .where(auctionImage.auction.id.eq(auction.id))
+                .orderBy(auctionImage.id.asc())
+                .limit(1),
+            auction.title,
+            auction.startPrice,
+            Expressions.constant(false),
+            auction.startedAt
+        ))
+        .from(auction)
+        .where(auction.status.eq(status))
+        .orderBy(auction.startedAt.asc())
+        .limit(limit)
+        .fetch();
+  }
+
+  @Override
+  public List<PopularInfo> getPopularInfo(AuctionStatus status, int limit){
+    return queryFactory
+        .select(Projections.constructor(PopularInfo.class,
+            auction.id,
+            JPAExpressions
+                .select(auctionImage.image)
+                .from(auctionImage)
+                .where(auctionImage.auction.id.eq(auction.id))
+                .orderBy(auctionImage.id.asc())
+                .limit(1),
+            auction.title,
+            auction.startPrice,
+            auction.currentPrice,
+            calculateDiscountRate(),
+            Expressions.constant(false),
+            auction.startedAt
+        ))
+        .from(auction)
+        .where(auction.status.eq(status))
+        .leftJoin(auctionPriceHistory)
+        .on(auction.id.eq(auctionPriceHistory.auction.id))
+        .orderBy(auctionPriceHistory.viewerCount.desc())
+        .limit(limit)
+        .fetch();
+  }
+
+  private NumberExpression<Long> calculateDiscountRate() {
+    return Expressions.numberTemplate(
+        Long.class,
+        "100 - ({0} * 100.0 / {1})",
+        auction.currentPrice,
+        auction.startPrice
+    );
+  }
+}
