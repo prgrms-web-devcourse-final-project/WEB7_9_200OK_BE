af84920d6a2d3d130786329c57223b8d9fbc855a
YEAJI YUN <dpwl0974@gmail.com>
2025-12-16 12:49:29 +0900

    [Feature] 스케줄러 기반 가격 하락 및 상태 변경 웹소켓 연결 (#43)
    * feature: 웹소켓 의존성 추가
    
    * feature: 웹소켓 설정 파일 추가
    
    * feature: 웹소켓 연결 로직 구현
    
    * feature: 경매 메시지 dto 생성
    
    * refactor: dto 방식으로 재구조화
    
    * refactor: 불필요한 임포트 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,11 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+// JSON 파싱
+
+    // websocket
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+
 
 }
 
