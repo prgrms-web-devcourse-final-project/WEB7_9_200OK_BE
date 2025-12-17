b6f057f70d1c40bab55774318622a14e43f988f2
Hodakrer <jihyuk0609@naver.com>
2025-12-17 16:11:44 +0900

    feature: 유저 로그인 잘못 올라간 테스트 코드 정상화

---
+++ src/test/java/com/windfall/api/auction/AuctionControllerTest.java
@@ -0,0 +1,535 @@
+package com.windfall.api.auction;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.windfall.api.auction.dto.request.AuctionCreateRequest;
+import com.windfall.domain.auction.entity.Auction;
+import com.windfall.domain.auction.enums.AuctionCategory;
+import com.windfall.domain.auction.enums.AuctionStatus;
+import com.windfall.domain.auction.repository.AuctionRepository;
+import com.windfall.domain.user.entity.User;
+import com.windfall.domain.user.enums.ProviderType;
+import com.windfall.domain.user.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class AuctionControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private AuctionRepository auctionRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private Long sellerId;
+
+  private Long auctionId;
+
+
+  @BeforeEach
+  void setUp() {
+    User seller = User.builder()
+        .email("test@naver.com")
+        .provider(ProviderType.NAVER)
+        .provideruserId("test1234")
+        .build();
+    User saveUser = userRepository.save(seller);
+    sellerId = saveUser.getId();
+
+    Auction auction = Auction.builder()
+        .title("테스트 제목")
+        .description("테스트 설명")
+        .category(AuctionCategory.DIGITAL)
+        .startPrice(10000L)
+        .currentPrice(10000L)
+        .stopLoss(9000L)
+        .dropAmount(50L)
+        .status(AuctionStatus.SCHEDULED)
+        .startedAt(LocalDateTime.now().plusDays(2))
+        .seller(saveUser)
+        .build();
+    Auction saveAuction = auctionRepository.save(auction);
+    auctionId = saveAuction.getId();
+  }
+  @Nested
+  @DisplayName("경매 생성 API")
+  class t1 {
+
+    @Test
+    @DisplayName("정상 작동")
+    void success() throws Exception{
+      // given
+      LocalDateTime resultTime = createTime();
+      String formattedTime = formated(resultTime);
+
+      AuctionCreateRequest request = new AuctionCreateRequest(
+          sellerId,
+          "테스트 제목",
+          "테스트 설명",
+          AuctionCategory.DIGITAL,
+          null,
+          10000L,
+          9000L,
+          50L,
+          resultTime
+      );
+
+      //when
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("createAuction"))
+          .andExpect(status().isCreated())
+          .andExpect(jsonPath("$.status").value("CREATED"))
+          .andExpect(jsonPath("$.message").value("경매가 생성되었습니다."))
+          .andExpect(jsonPath("$.data.sellerId").value(sellerId))
+          .andExpect(jsonPath("$.data.title").value("테스트 제목"))
+          .andExpect(jsonPath("$.data.description").value("테스트 설명"))
+          .andExpect(jsonPath("$.data.category").value(AuctionCategory.DIGITAL.name()))
+          .andExpect(jsonPath("$.data.startPrice").value(10000L))
+          .andExpect(jsonPath("$.data.currentPrice").value(10000L))
+          .andExpect(jsonPath("$.data.stopLoss").value(9000L))
+          .andExpect(jsonPath("$.data.dropAmount").value(50L))
+          .andExpect(jsonPath("$.data.status").value(AuctionStatus.SCHEDULED.name()))
+          .andExpect(jsonPath("$.data.startAt").value(formattedTime))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Validation 예외처리 상황 - 제목")
+    void fail1() throws Exception{
+      // given
+      LocalDateTime resultTime = createTime();
+
+      AuctionCreateRequest request = new AuctionCreateRequest(
+          sellerId,
+          null,
+          "테스트 설명",
+          AuctionCategory.DIGITAL,
+          null,
+          10000L,
+          9000L,
+          50L,
+          resultTime
+      );
+
+      //when
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("createAuction"))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+          .andExpect(jsonPath("$.message").value("경매 제목은 필수입니다."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("경매 가격 하락가가 시작가의 0.5% 이하일 때")
+    void fail2() throws Exception{
+      // given
+      LocalDateTime resultTime = createTime();
+
+      AuctionCreateRequest request = new AuctionCreateRequest(
+          sellerId,
+          "테스트 제목",
+          "테스트 설명",
+          AuctionCategory.DIGITAL,
+          null,
+          10000L,
+          9000L,
+          49L,
+          resultTime
+      );
+
+      //when
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("createAuction"))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+          .andExpect(jsonPath("$.message").value("경매 하락 가격을 다시 설정해주세요."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("경매 스탑로스가 시작가의 90% 초과일 때")
+    void fail3() throws Exception{
+      // given
+      LocalDateTime resultTime = createTime();
+
+      AuctionCreateRequest request = new AuctionCreateRequest(
+          sellerId,
+          "테스트 제목",
+          "테스트 설명",
+          AuctionCategory.DIGITAL,
+          null,
+          10000L,
+          9001L,
+          50L,
+          resultTime
+      );
+
+      //when
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("createAuction"))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+          .andExpect(jsonPath("$.message").value("경매 Stop Loss을 다시 설정해주세요."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("경매 시작 시간이 1일 이내일 때")
+    void fail4() throws Exception{
+      // given
+      LocalDateTime resultTime = createTime().minusMinutes(10);
+
+      AuctionCreateRequest request = new AuctionCreateRequest(
+          sellerId,
+          "테스트 제목",
+          "테스트 설명",
+          AuctionCategory.DIGITAL,
+          null,
+          10000L,
+          9000L,
+          50L,
+          resultTime
+      );
+
+      //when
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("createAuction"))
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+          .andExpect(jsonPath("$.message").value("경매 시간을 다시 설정해주세요."))
+          .andDo(print());
+    }
+  }
+
+  @Nested
+  @DisplayName("경매 삭제 API")
+  class t2 {
+    @Test
+    @DisplayName("정상 작동")
+    void success() throws Exception{
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          delete("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("deleteAuction"))
+          .andExpect(status().isNoContent())
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 경매일 때")
+    void fail1() throws Exception{
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          delete("/api/v1/auctions/%s".formatted(9999L))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("deleteAuction"))
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+          .andExpect(jsonPath("$.message").value("존재하지 않는 경매입니다."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("경매 판매자가 아닐 때")
+    void fail2() throws Exception{
+      User seller = User.builder()
+          .email("test@naver.com")
+          .provider(ProviderType.NAVER)
+          .provideruserId("test1234")
+          .build();
+      User notUser = userRepository.save(seller);
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          delete("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(notUser.getId()))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("deleteAuction"))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.status").value("FORBIDDEN"))
+          .andExpect(jsonPath("$.message").value("해당 경매의 판매자가 아닙니다."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("진행 중인 경매일 때")
+    void fail3() throws Exception{
+      // given
+      User user = userRepository.findById(sellerId).orElse(null);
+
+      Auction auction = Auction.builder()
+          .title("진행 중인 경매")
+          .description("테스트 설명")
+          .category(AuctionCategory.DIGITAL)
+          .startPrice(10000L)
+          .currentPrice(9500L)
+          .stopLoss(9000L)
+          .dropAmount(50L)
+          .status(AuctionStatus.PROCESS)
+          .startedAt(LocalDateTime.now().minusHours(1))
+          .seller(user)
+          .build();
+
+      auctionRepository.save(auction);
+      auctionId = auction.getId();
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          delete("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("deleteAuction"))
+          .andExpect(status().isConflict())
+          .andExpect(jsonPath("$.status").value("CONFLICT"))
+          .andExpect(jsonPath("$.message").value("현재 상태의 경매는 삭제할 수 없습니다."))
+          .andDo(print());
+
+    }
+  }
+
+  @Nested
+  @DisplayName("경매 취소 API")
+  class t3{
+    @Test
+    @DisplayName("정상 작동")
+    void success() throws Exception{
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          patch("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("cancelAuction"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.status").value("OK"))
+          .andExpect(jsonPath("$.message").value("경매가 취소되었습니다."))
+          .andExpect(jsonPath("$.data.auctionId").value(auctionId))
+          .andExpect(jsonPath("$.data.status").value(AuctionStatus.CANCELED.name()))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 경매일 때")
+    void fail1() throws Exception{
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          patch("/api/v1/auctions/%s".formatted(9999L))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("cancelAuction"))
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+          .andExpect(jsonPath("$.message").value("존재하지 않는 경매입니다."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("경매 판매자가 아닐 때")
+    void fail2() throws Exception{
+      User seller = User.builder()
+          .email("test@naver.com")
+          .provider(ProviderType.NAVER)
+          .provideruserId("test1234")
+          .build();
+      User notUser = userRepository.save(seller);
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          patch("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(notUser.getId()))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("cancelAuction"))
+          .andExpect(status().isForbidden())
+          .andExpect(jsonPath("$.status").value("FORBIDDEN"))
+          .andExpect(jsonPath("$.message").value("해당 경매의 판매자가 아닙니다."))
+          .andDo(print());
+    }
+
+    @Test
+    @DisplayName("진행 중인 경매일 때")
+    void fail3() throws Exception{
+      // given
+      User user = userRepository.findById(sellerId).orElse(null);
+
+      Auction auction = Auction.builder()
+          .title("진행 중인 경매")
+          .description("테스트 설명")
+          .category(AuctionCategory.DIGITAL)
+          .startPrice(10000L)
+          .currentPrice(9500L)
+          .stopLoss(9000L)
+          .dropAmount(50L)
+          .status(AuctionStatus.PROCESS)
+          .startedAt(LocalDateTime.now().minusHours(1))
+          .seller(user)
+          .build();
+
+      auctionRepository.save(auction);
+      auctionId = auction.getId();
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          patch("/api/v1/auctions/%s".formatted(auctionId))
+              .param("userId", String.valueOf(sellerId))  // TODO : 추후 로그인 개발 시 제거 필요
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("cancelAuction"))
+          .andExpect(status().isConflict())
+          .andExpect(jsonPath("$.status").value("CONFLICT"))
+          .andExpect(jsonPath("$.message").value("현재 상태의 경매는 취소할 수 없습니다."))
+          .andDo(print());
+
+    }
+  }
+
+  @Nested
+  @DisplayName("경매 목록 조회 API")
+  class t4 {
+    @Test
+    @DisplayName("정상 작동")
+    void success() throws Exception{
+      // when
+      ResultActions resultActions = mockMvc.perform(
+          get("/api/v1/auctions")
+              .accept(MediaType.APPLICATION_JSON)
+      );
+
+      // then
+      resultActions
+          .andExpect(handler().handlerType(AuctionController.class))
+          .andExpect(handler().methodName("readAuctionList"))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.status").value("OK"))
+          .andExpect(jsonPath("$.message").value("경매 목록 조회에 성공했습니다."))
+          .andDo(print());
+    }
+  }
+
+  private LocalDateTime createTime(){
+    LocalDateTime now = LocalDateTime.now().plusMinutes(5);
+
+    int minute = now.getMinute();
+    int remainder = minute % 5;
+
+    int baseMinute = (remainder == 0 ? minute : minute + (5 - remainder));
+
+    LocalDateTime resultTime = now.withMinute(baseMinute % 60);
+
+    if(baseMinute >= 60){
+      resultTime = resultTime.plusHours(1);
+    }
+
+    return resultTime.plusDays(1);
+  }
+  private String formated(LocalDateTime resultTime){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    return resultTime.format(formatter);
+  }
+}
---
+++ src/test/java/com/windfall/api/tag/TagServiceTest.java
@@ -0,0 +1,190 @@
+package com.windfall.api.tag;
+
+import static com.windfall.global.exception.ErrorCode.TAG_CONTAINS_SPACE;
+import static com.windfall.global.exception.ErrorCode.TAG_EMPTY;
+import static com.windfall.global.exception.ErrorCode.TAG_INVALID_CHAR;
+import static com.windfall.global.exception.ErrorCode.TAG_TOO_LONG;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.windfall.domain.auction.entity.Auction;
+import com.windfall.domain.auction.enums.AuctionCategory;
+import com.windfall.domain.auction.enums.AuctionStatus;
+import com.windfall.domain.auction.repository.AuctionRepository;
+import com.windfall.domain.tag.entity.AuctionTag;
+import com.windfall.domain.tag.entity.Tag;
+import com.windfall.domain.tag.repository.AuctionTagRepository;
+import com.windfall.domain.tag.repository.TagRepository;
+import com.windfall.domain.user.entity.User;
+import com.windfall.domain.user.enums.ProviderType;
+import com.windfall.domain.user.repository.UserRepository;
+import com.windfall.global.exception.ErrorException;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class TagServiceTest {
+
+  @Autowired
+  private TagService tagService;
+
+  @Autowired
+  private AuctionRepository auctionRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private TagRepository tagRepository;
+
+  @Autowired
+  private AuctionTagRepository auctionTagRepository;
+
+  //@Mock
+  //private TagSearchRepository tagSearchRepository; // ES Repository를 Mock으로 대체
+
+  private Auction auction;
+
+  @BeforeEach
+  void setUp() {
+    User seller = userRepository.save(User.builder()
+        .email("test@naver.com")
+        .provider(ProviderType.NAVER)
+        .provideruserId("test1234")
+        .build()
+    );
+
+    auction = Auction.builder()
+        .title("테스트 제목")
+        .description("테스트 설명")
+        .category(AuctionCategory.DIGITAL)
+        .startPrice(10000L)
+        .currentPrice(10000L)
+        .stopLoss(9000L)
+        .dropAmount(50L)
+        .status(AuctionStatus.SCHEDULED)
+        .startedAt(LocalDateTime.now().plusDays(2))
+        .seller(seller)
+        .build();
+
+    auction = auctionRepository.save(auction);
+  }
+
+  @Test
+  @DisplayName("태그 등록 후 Tag와 AuctionTag에 값이 저장되는 경우")
+  void success1() {
+    // given
+    List<String> tags = List.of("고기", "고등어", "군고구마", "고등어");
+
+    // when
+    tagService.registerAuctionTags(auction, tags);
+
+    // then
+    List<Tag> savedTags = tagRepository.findAll();
+    assertEquals(3, savedTags.size());
+
+    List<AuctionTag> auctionTags = auctionTagRepository.findAll();
+    assertEquals(4, auctionTags.size());
+  }
+
+  @Test
+  @DisplayName("사용자가 등록한 태그가 없는 경우")
+  public void success2() {
+    //given
+    List<String> tag1 = null;
+    List<String> tag2 = List.of();
+
+    //when
+    tagService.registerAuctionTags(auction, tag1);
+    tagService.registerAuctionTags(auction, tag2);
+
+    //then
+    List<Tag> savedTags = tagRepository.findAll();
+    assertEquals(0, savedTags.size());
+
+    List<AuctionTag> auctionTags = auctionTagRepository.findAll();
+    assertEquals(0, auctionTags.size());
+  }
+
+  @Test
+  @DisplayName("태그에 공백이 있는 경우")
+  public void exception1() {
+    //given
+    List<String> tags = List.of("가방", "고구마", "식 탁");
+
+    // when & then
+    ErrorException exception = assertThrows(
+        ErrorException.class,
+        () -> tagService.registerAuctionTags(auction, tags)
+    );
+
+    assertEquals(TAG_CONTAINS_SPACE, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("단일 태그의 값이 없는 경우")
+  public void exception3() {
+    //given
+    List<String> tags = List.of("가방", "고구마", "", "식탁");
+
+    // when & then
+    ErrorException exception = assertThrows(
+        ErrorException.class,
+        () -> tagService.registerAuctionTags(auction, tags)
+    );
+
+    assertEquals(TAG_EMPTY, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("태그에 허용되지 않은 문자가 있는 경우1")
+  public void exception4() {
+    //given
+    List<String> tags = List.of("가%방", "고구마", "식탁");
+
+    // when & then
+    ErrorException exception = assertThrows(
+        ErrorException.class,
+        () -> tagService.registerAuctionTags(auction, tags)
+    );
+
+    assertEquals(TAG_INVALID_CHAR, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("태그에 허용되지 않은 문자가 있는 경우2")
+  public void exception5() {
+    //given
+    List<String> tags = List.of("가방", "고구마🍠", "식탁");
+
+    // when & then
+    ErrorException exception = assertThrows(
+        ErrorException.class,
+        () -> tagService.registerAuctionTags(auction, tags)
+    );
+
+    assertEquals(TAG_INVALID_CHAR, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("태그가 최대 글자 수를 초과한 경우")
+  public void exception6() {
+    //given
+    List<String> tags = List.of("가방", "고구마진짜맛있어요꼭사세요", "식탁");
+
+    // when & then
+    ErrorException exception = assertThrows(
+        ErrorException.class,
+        () -> tagService.registerAuctionTags(auction, tags)
+    );
+
+    assertEquals(TAG_TOO_LONG, exception.getErrorCode());
+  }
+}
