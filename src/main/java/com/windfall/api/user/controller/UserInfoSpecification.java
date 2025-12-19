279bb0523ea012fd7d741ffac62554d223fb2b43
DEV-Cheeze <98104426+DEV-Cheeze@users.noreply.github.com>
2025-12-19 16:04:11 +0900

    [Feature] 사용자 정보 조회 api 개발 및 테스트코드 작성 (#63)
    * added UserInfoSpecfication for swagger (Beta)
    
    * refactor: updated UserInfoSpecfication for swagger (Beta)
    
    * refactor: updated UserInfoSpecfication for swagger (Beta)
    
    * refactor: added param of myid to UserInfoSpecfication getUserInfo method (Beta)
    
    * feat: added JPQL Query  for findByUserInfo
    
    * feat: added UserInfoAPIs
    
    * feat: added UserinfoController.java
    
    * feat: added UserinfoService.java
    
    * feat: added JPQL Query to UserRepository.java
    
    * refactor: added Swagger Schema description for UserInfoResponse
    
    * test: added UserInfoController Test
    
    * refactor: updated UserInfoControllerTest.java

---
+++ src/main/java/com/windfall/api/user/controller/UserInfoSpecification.java
@@ -0,0 +1,16 @@
+package com.windfall.api.user.controller;
+
+import com.windfall.api.user.dto.response.UserInfoResponse;
+import com.windfall.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "UserInfo", description = "사용자 정보 API")
+public interface UserInfoSpecification {
+
+  @Operation(summary = "사용자 정보", description = "특정 사용자의 정보를 반환합니다.")
+  ApiResponse<UserInfoResponse> getUserInfo(@PathVariable Long userid, @RequestParam Long loginId);
+
+}
