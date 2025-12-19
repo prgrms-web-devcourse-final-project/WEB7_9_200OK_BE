8a9e0d7da413b15b788af562d2568d60cce92fdf
DEV-Cheeze <ckdwnd9999@gmail.com>
2025-12-19 10:49:06 +0900

    feat: added JPQL Query  for findByUserInfo

--- src/main/java/com/windfall/domain/user/repository/UserRepository.java
+++ src/main/java/com/windfall/domain/user/repository/UserRepository.java
@@ -1,9 +1,32 @@
 package com.windfall.domain.user.repository;
 
+import com.windfall.api.user.dto.response.UserInfoResponse;
+import com.windfall.domain.trade.enums.TradeStatus;
 import com.windfall.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByProviderUserId(String providerUserId);
+
+  @Query("""
+  SELECT
+  new com.windfall.api.user.dto.response.UserInfoResponse(
+  (u.id = :loginId),
+  u.id,
+  u.nickname,
+  u.email,
+  u.profileImageUrl,
+  COUNT(r.id),
+  COALESCE(AVG(r.rating), 0.0)
+  )
+  FROM User u
+  LEFT JOIN Trade t ON u.id = t.sellerId AND t.status = "PURCHASE_CONFIRMED"
+  LEFT JOIN Review r ON r.trade.id = t.id
+  WHERE u.id = :id
+  GROUP BY u.id, u.nickname, u.email, u.profileImageUrl
+""")
+  UserInfoResponse findByUserInfo(@Param("id") Long id, @Param("loginId") Long loginId);
 }
