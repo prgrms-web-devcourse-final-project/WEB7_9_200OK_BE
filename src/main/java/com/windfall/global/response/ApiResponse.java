d7c2065d572587214564c139a4674a7a38e2d260
MyoungJin Seo <80959635+myoungjinseo@users.noreply.github.com>
2025-12-19 10:22:37 +0900

    refactor: 상태 코드 값 추가 #59 (#60)

--- src/main/java/com/windfall/global/response/ApiResponse.java
+++ src/main/java/com/windfall/global/response/ApiResponse.java
@@ -5,12 +5,13 @@ import com.windfall.global.exception.ErrorException;
 import org.springframework.http.HttpStatus;
 
 public record ApiResponse<T>(
+        int code,
         HttpStatus status,
         String message,
         T data
 ) {
     public static <T> ApiResponse<T> ok( String message, T data) {
-        return new ApiResponse<>(HttpStatus.OK, message, data);
+        return new ApiResponse<>(HttpStatus.OK.value(),HttpStatus.OK, message, data);
     }
 
     public static <T> ApiResponse<T> ok(T data) {
@@ -18,16 +19,17 @@ public record ApiResponse<T>(
     }
 
     public static <T> ApiResponse<T> created( String message, T data) {
-        return new ApiResponse<>(HttpStatus.CREATED, message, data);
+        return new ApiResponse<>(HttpStatus.CREATED.value(),HttpStatus.CREATED, message, data);
     }
 
     public static <T> ApiResponse<T> noContent() {
-        return new ApiResponse<>(HttpStatus.NO_CONTENT, null, null);
+        return new ApiResponse<>(HttpStatus.NO_CONTENT.value(),HttpStatus.NO_CONTENT, null, null);
     }
 
     public static ApiResponse<?> fail(ErrorException errorException) {
         ErrorCode errorCode = errorException.getErrorCode();
         return new ApiResponse<>(
+                errorCode.getHttpStatus().value(),
                 errorCode.getHttpStatus(),
                 errorCode.getMessage(),
                 null
@@ -36,6 +38,7 @@ public record ApiResponse<T>(
 
     public static ApiResponse<?> fail(HttpStatus status, String message) {
         return new ApiResponse<>(
+                status.value(),
                 status,
                 message,
                 null
