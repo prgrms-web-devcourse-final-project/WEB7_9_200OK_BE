d9138c9036b65d27c22045404001bfb20d3874fb
Ho-JunYu <hojoon1011@gmail.com>
2025-12-19 14:52:45 +0900

    feat: CORS preflight(OPTIONS) 요청은 무조건 filterChain.doFilter()로 통과시키는 로직 추가

--- src/main/java/com/windfall/global/config/security/CustomAuthenticationFilter.java
+++ src/main/java/com/windfall/global/config/security/CustomAuthenticationFilter.java
@@ -27,6 +27,12 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
     logger.debug("CustomAuthenticationFilter called");
 
     try {
