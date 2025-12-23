af5ea0cd4321ee0650d71244e1c765fca96e51d3
DEV-Cheeze <ckdwnd9999@gmail.com>
2025-12-24 01:16:37 +0900

    feat: added UserSalesHistory Swagger docs and Implemented UserSalesHistory Controller logics to UserInfoService

--- src/main/java/com/windfall/api/user/controller/UserInfoSpecification.java
+++ src/main/java/com/windfall/api/user/controller/UserInfoSpecification.java
@@ -1,9 +1,13 @@
 package com.windfall.api.user.controller;
 
 import com.windfall.api.user.dto.response.UserInfoResponse;
+import com.windfall.api.user.dto.response.saleshistory.BaseSalesHistoryResponse;
 import com.windfall.global.response.ApiResponse;
+import com.windfall.global.response.SliceResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,5 +16,14 @@ public interface UserInfoSpecification {
 
   @Operation(summary = "사용자 정보", description = "특정 사용자의 정보를 반환합니다.")
   ApiResponse<UserInfoResponse> getUserInfo(@PathVariable Long userid, @RequestParam Long loginId);
+
+  @Operation(summary = "사용자 판매 내역", description = "특정 사용자의 판매 내역을 반환합니다.")
+  ApiResponse<SliceResponse<BaseSalesHistoryResponse>> getUserSalesHistory(
+      @PathVariable Long userid,
+      @RequestParam Long loginId,
+      @RequestParam(required = false) String filter,
+      @PageableDefault(page = 0, size = 10) Pageable pageable
+
+  );
 
 }
