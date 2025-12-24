d13e3ebcbd322489896bfa3172f6ae300fcae488
Hodakrer <jihyuk0609@naver.com>
2025-12-24 09:41:06 +0900

    feature: 유저 로그인 오류 수정

---
+++ src/main/java/com/windfall/api/user/controller/OAuthExchangeSpecification.java
@@ -0,0 +1,12 @@
+package com.windfall.api.user.controller;
+
+import com.windfall.api.user.dto.request.OAuthTokenRequest;
+import com.windfall.api.user.dto.response.OAuthTokenResponse;
+import com.windfall.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface OAuthExchangeSpecification {
+  @Operation(summary = "카카오 로그인 익스체인지 요청", description = "카카오 콜백에서 받은 코드로 요청하면 액세스토큰, 리프레시토큰 스트링을 반환합니다.")
+  ApiResponse<OAuthTokenResponse> kakaoExchange(@RequestBody OAuthTokenRequest request);
+}
---
+++ src/main/java/com/windfall/domain/user/entity/UserToken.java
@@ -0,0 +1,36 @@
+package com.windfall.domain.user.entity;
+
+import com.windfall.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserToken  extends BaseEntity {
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false, unique = true)
+  private User user;
+
+  @Column(nullable = false)
+  private String refreshToken;
+
+  protected UserToken(User user, String refreshToken) {
+    this.user = user;
+    this.refreshToken = refreshToken;
+  }
+
+  public static UserToken create(User user, String refreshToken) {
+    return new UserToken(user, refreshToken);
+  }
+  public void saveRefreshToken(String newRefreshToken) {
+    this.refreshToken = newRefreshToken;
+  }
+}
