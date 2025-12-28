213eaaba49a4b336706334aa69fece9ffb508899
Ho-JunYu <hojoon1011@gmail.com>
2025-12-28 16:29:36 +0900

    feat: 채팅방 상세 조회 API 컨트롤러 구현

--- src/main/java/com/windfall/api/chat/controller/ChatRoomController.java
+++ src/main/java/com/windfall/api/chat/controller/ChatRoomController.java
@@ -1,12 +1,16 @@
 package com.windfall.api.chat.controller;
 
 import com.windfall.api.chat.dto.request.enums.ChatRoomScope;
+import com.windfall.api.chat.dto.response.ChatRoomDetailResponse;
 import com.windfall.api.chat.dto.response.ChatRoomListResponse;
 import com.windfall.api.chat.service.ChatRoomService;
+import com.windfall.domain.user.entity.CustomUserDetails;
 import com.windfall.global.response.ApiResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +31,19 @@ public class ChatRoomController implements ChatRoomSpecification {
 
     List<ChatRoomListResponse> response = chatRoomService.getChatRooms(userId, scope);
     return ApiResponse.ok("채팅방 목록이 조회되었습니다.", response);
+  }
+
+  @Override
+  @GetMapping("/{chatRoomId}/messages")
+  public ApiResponse<ChatRoomDetailResponse> getChatRoomDetail(
+      @PathVariable Long chatRoomId,
+      @RequestParam(required = false) Long cursor,
+      @RequestParam(defaultValue = "20") int size,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    Long userId = userDetails.getUserId();
+    ChatRoomDetailResponse response = chatRoomService.getChatRoomDetail(userId, chatRoomId, cursor,
+        size);
+    return ApiResponse.ok("채팅방 상세 정보가 조회되었습니다.", response);
   }
 }
