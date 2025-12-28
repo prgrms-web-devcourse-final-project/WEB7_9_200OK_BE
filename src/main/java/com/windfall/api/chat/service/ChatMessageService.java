c5451cf2c814f86d920c4dabf9609878c92b423e
Ho-JunYu <hojoon1011@gmail.com>
2025-12-29 01:24:41 +0900

    feat: ChatMessageService - 유저 검증 로직 및 채팅방 조회 쿼리 추가

--- src/main/java/com/windfall/api/chat/service/ChatMessageService.java
+++ src/main/java/com/windfall/api/chat/service/ChatMessageService.java
@@ -1,9 +1,16 @@
 package com.windfall.api.chat.service;
 
+import com.windfall.api.chat.dto.response.ChatReadMarkResponse;
+import com.windfall.api.user.service.UserService;
+import com.windfall.domain.chat.entity.ChatRoom;
 import com.windfall.domain.chat.repository.ChatMessageRepository;
 import com.windfall.domain.chat.repository.ChatRoomRepository;
+import com.windfall.domain.user.entity.User;
+import com.windfall.global.exception.ErrorCode;
+import com.windfall.global.exception.ErrorException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +18,16 @@ public class ChatMessageService {
 
   private final ChatRoomRepository chatRoomRepository;
   private final ChatMessageRepository chatMessageRepository;
+  private final UserService userService;
+
+  @Transactional
+  public ChatReadMarkResponse markAsRead(Long chatRoomId, Long userId) {
+
+    User me = userService.getUserById(userId);
+
+    ChatRoom chatRoom = chatRoomRepository.findByIdWithTrade(chatRoomId)
+        .orElseThrow(() -> new ErrorException(ErrorCode.NOT_FOUND_CHAT_ROOM));
+
+  }
 
 }
