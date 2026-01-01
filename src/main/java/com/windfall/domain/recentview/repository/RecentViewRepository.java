ae56d8b094da165fdaa3eb690c75a076bb8abeab
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-01 19:07:58 +0900

    feat: Added RecentViewRepository

--- src/main/java/com/windfall/domain/recentview/repository/RecentViewRepository.java
+++ src/main/java/com/windfall/domain/recentview/repository/RecentViewRepository.java
@@ -1,8 +1,17 @@
 package com.windfall.domain.recentview.repository;
 
 import com.windfall.domain.recentview.entity.RecentView;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RecentViewRepository extends JpaRepository<RecentView, Long> {
 
+  RecentView findByAuctionIdAndUserId(Long auctionId, Long userId);
+
+  boolean existsByAuctionIdAndUserId(Long auctionId, Long userId);
+
+  int countByUserId(Long userId);
+
+  RecentView findTop1ByUserIdOrderByViewedAt(Long userId);
 }
