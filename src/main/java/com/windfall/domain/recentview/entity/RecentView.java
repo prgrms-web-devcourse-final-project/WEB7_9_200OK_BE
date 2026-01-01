112645f36938a0aa1e17df52efd50c7db5415ca1
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-01 19:03:45 +0900

    refactor: add column viewed_at

--- src/main/java/com/windfall/domain/recentview/entity/RecentView.java
+++ src/main/java/com/windfall/domain/recentview/entity/RecentView.java
@@ -10,6 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,16 +37,25 @@ public class RecentView extends BaseEntity {
   @Column(name = "user_id", nullable = false)
   private Long userId;
 
+  @Column(name = "viewed_at", nullable = false)
+  private LocalDateTime viewedAt;
+
   @Builder
-  public RecentView(Auction auction, Long userId) {
+  public RecentView(Auction auction, Long userId, LocalDateTime viewedAt) {
     this.auction = auction;
     this.userId = userId;
+    this.viewedAt = viewedAt;
   }
 
-  public static RecentView create(Auction auction, Long userId){
+  public static RecentView create(Auction auction, Long userId, LocalDateTime viewedAt){
     return RecentView.builder()
         .auction(auction)
         .userId(userId)
+        .viewedAt(viewedAt)
         .build();
+  }
+
+  public void updateView(){
+    this.viewedAt = LocalDateTime.now();
   }
 }
