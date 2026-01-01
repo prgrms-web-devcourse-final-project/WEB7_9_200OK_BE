877c19642f6665352c0079b0786cfc40e3cbd381
Ho-JunYu <hojoon1011@gmail.com>
2026-01-01 20:06:24 +0900

    feat: 채팅 메시지 전송용 웹소켓 컨트롤러(ChatWsController) 추가

---
+++ src/main/java/com/windfall/api/chat/controller/websocket/ChatWsController.java
@@ -0,0 +1,21 @@
+package com.windfall.api.chat.controller.websocket;
+
+import com.windfall.api.chat.dto.websocket.ChatSendRequest;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatWsController {
+
+  private final ChatWsService chatWsService;
+
+  @MessageMapping("/chat.send")
+  public void send(ChatSendRequest request, Principal principal) {
+    Long userId = Long.valueOf(principal.getName());
+    chatWsService.sendChatMessage(userId, request);
+  }
+
+}
