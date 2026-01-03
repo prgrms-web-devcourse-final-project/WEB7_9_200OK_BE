3bdf864a5c7c331657f10e588de26d676e16ac46
Ho-JunYu <hojoon1011@gmail.com>
2026-01-03 22:29:15 +0900

    feat: 채팅 이미지 업로드 API 명세(ChatImageSpecification) 추가

---
+++ src/main/java/com/windfall/api/chat/controller/ChatImageSpecification.java
@@ -0,0 +1,27 @@
+package com.windfall.api.chat.controller;
+
+import com.windfall.api.chat.dto.response.ChatImageUploadResponse;
+import com.windfall.domain.user.entity.CustomUserDetails;
+import com.windfall.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "Chat Image", description = "채팅 이미지 업로드 API")
+public interface ChatImageSpecification {
+
+  @Operation(
+      summary = "채팅 이미지 업로드",
+      description = """
+          채팅용 이미지를 S3에 업로드하고 URL 목록을 반환합니다.
+          반환된 URL로 WebSocket IMAGE 메시지를 전송하세요.
+          """
+  )
+  ApiResponse<List<ChatImageUploadResponse>> uploadChatImages(
+      @RequestPart(name = "uploadFiles") List<MultipartFile> files,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  );
+}
