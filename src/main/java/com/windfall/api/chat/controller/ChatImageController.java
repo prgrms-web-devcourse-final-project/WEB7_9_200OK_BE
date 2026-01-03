9e383b569c88729feb31d3af4d35f193f01e1efc
Ho-JunYu <hojoon1011@gmail.com>
2026-01-03 23:51:45 +0900

    feat: 채팅 이미지 업로드 API 컨트롤러 구현

---
+++ src/main/java/com/windfall/api/chat/controller/ChatImageController.java
@@ -0,0 +1,33 @@
+package com.windfall.api.chat.controller;
+
+import com.windfall.api.chat.dto.response.ChatImageUploadResponse;
+import com.windfall.domain.user.entity.CustomUserDetails;
+import com.windfall.global.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/chat-images")
+public class ChatImageController implements ChatImageSpecification{
+
+  private final ChatImageService chatImageService;
+
+  @Override
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ApiResponse<List<ChatImageUploadResponse>> uploadChatImages(
+      @RequestPart(name = "uploadFiles") List<MultipartFile> files,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    Long userId = userDetails.getUserId();
+    List<ChatImageUploadResponse> response = chatImageService.uploadChatImages(userId, files);
+    return ApiResponse.ok("채팅 이미지가 업로드 되었습니다.", response);
+  }
+}
