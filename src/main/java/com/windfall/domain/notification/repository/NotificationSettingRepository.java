781cd142b2f900b2d927951f474c38cc9a72dd20
dpwl0974 <dpwl0974@gmail.com>
2026-01-03 14:17:20 +0900

    feature: 경매 시작 알림 활성화에 따른 조회 로직 구현

--- src/main/java/com/windfall/domain/notification/repository/NotificationSettingRepository.java
+++ src/main/java/com/windfall/domain/notification/repository/NotificationSettingRepository.java
@@ -5,6 +5,7 @@ import com.windfall.domain.notification.enums.NotificationSettingType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
 
@@ -12,4 +13,13 @@ public interface NotificationSettingRepository extends JpaRepository<Notificatio
       NotificationSettingType type);
 
   List<NotificationSetting> findByUserIdAndAuctionId(Long userId, Long auctionId);
+
+  @Query("""
+      select n from NotificationSetting n
+      join fetch n.user
+      where n.auction.id = :auctionId
+        and n.type = :type
+        and n.activated = true
+      """)
+  List<NotificationSetting> findAllActiveByAuctionAndType(Long auctionId, NotificationSettingType type);
 }
