403afc512dab0302e89312bab0e599c5d2ace9e7
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-04 16:39:02 +0900

    feat: Added ReviewResponseDTOS

---
+++ src/main/java/com/windfall/api/review/dto/response/CreateReviewResponse.java
@@ -0,0 +1,25 @@
+package com.windfall.api.review.dto.response;
+
+import com.windfall.domain.review.entity.Review;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+public record CreateReviewResponse(
+    @Schema(description = "리뷰 ID")
+    Long reviewId,
+
+    @Schema(description = "별점")
+    int rating,
+
+    @Schema(description = "내용")
+    String content
+) {
+  public static CreateReviewResponse from(Review review){
+    return CreateReviewResponse.builder()
+        .reviewId(review.getId())
+        .rating(review.getRating() / 10)
+        .content(review.getContent())
+        .build();
+  }
+}
---
+++ src/main/java/com/windfall/api/review/dto/response/ReviewDetailsResponse.java
@@ -0,0 +1,44 @@
+package com.windfall.api.review.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+public record ReviewDetailsResponse(
+    @Schema(description = "경매 ID")
+    Long auctionId,
+
+    @Schema(description = "리뷰 ID")
+    Long reviewId,
+
+    @Schema(description = "판매자 ID")
+    Long sellerId,
+
+    @Schema(description = "경매 이름 (상품 이름)")
+    String auctionTitle,
+
+    @Schema(description = "경매 썸네일 이미지")
+    String auctionImageUrl,
+
+    @Schema(description = "판매자 이름")
+    String nickname,
+
+    @Schema(description = "별점")
+    int rating,
+
+    @Schema(description = "내용")
+    String content
+  ){
+  public static ReviewDetailsResponse of(ReviewDetailsRaw rawReview, String auctionImageUrl){
+    return ReviewDetailsResponse.builder()
+        .auctionId(rawReview.auctionId())
+        .reviewId(rawReview.reviewId())
+        .sellerId(rawReview.sellerId())
+        .auctionImageUrl(auctionImageUrl)
+        .auctionTitle(rawReview.auctionTitle())
+        .content(rawReview.content())
+        .rating(rawReview.rating())
+        .nickname(rawReview.nickname())
+        .build();
+  }
+}
