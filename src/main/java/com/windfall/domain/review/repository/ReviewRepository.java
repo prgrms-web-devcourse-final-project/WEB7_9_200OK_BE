88dda1695d31ab719d66de2aaaacbbfebef2f10b
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-04 16:40:16 +0900

    Added ReviewRepository Queries

--- src/main/java/com/windfall/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/windfall/domain/review/repository/ReviewRepository.java
@@ -1,8 +1,26 @@
 package com.windfall.domain.review.repository;
 
+import com.windfall.api.review.dto.response.ReviewDetailsRaw;
+import com.windfall.api.review.dto.response.ReviewDetailsResponse;
 import com.windfall.domain.review.entity.Review;
+import com.windfall.domain.trade.entity.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+  boolean existsReviewByTradeId(Long tradeId);
+
+  @Query("""
+  SELECT new com.windfall.api.review.dto.response.ReviewDetailsRaw(
+    a.id, r.id, s.id, a.title, s.nickname, r.rating / 10, r.content
+    )
+  FROM Review r
+  JOIN r.trade t
+  JOIN t.auction a
+  JOIN a.seller s
+  WHERE r.id = :id
+  """)
+  ReviewDetailsRaw findReviewDetails(@Param("id") Long reviewId);
 }
