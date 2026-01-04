ba9460f2583405a91153fa5e1507943e2a2f7514
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-04 16:38:42 +0900

    feat: Added ReviewRequestDTOS (Create, Update)

--- src/main/java/com/windfall/api/review/dto/request/CreateReviewRequest.java
+++ src/main/java/com/windfall/api/review/dto/request/CreateReviewRequest.java
@@ -1,5 +1,19 @@
 package com.windfall.api.review.dto.request;
 
-public record CreateReviewRequest() {
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+public record CreateReviewRequest(
+    @NotNull(message = "거래 id는 필수입니다.")
+    Long tradeId,
+
+    @NotNull(message = "별점은 필수로 입력되어야 합니다.")
+    @Range(min=1, max=5, message = "별점은 최소 1점 최대 5점까지 주어져야 합니다.")
+    Integer rating,
+
+    @Length(max = 500, message = "리뷰 내용은 최대 500자 까지만 입력이 가능합니다.")
+    String content
+) {
 
 }
