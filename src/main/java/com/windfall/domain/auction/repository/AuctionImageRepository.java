517aa46166a54a770dde041c8790fa3f9a7f9c5c
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-04 16:50:52 +0900

    feat: Added AuctionThumbnail Projection Query in AuctionImageRepository

--- src/main/java/com/windfall/domain/auction/repository/AuctionImageRepository.java
+++ src/main/java/com/windfall/domain/auction/repository/AuctionImageRepository.java
@@ -1,5 +1,6 @@
 package com.windfall.domain.auction.repository;
 
+import com.windfall.api.user.dto.response.reviewlist.AuctionImageRaw;
 import com.windfall.domain.auction.entity.AuctionImage;
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,19 @@ public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long
       )
       """)
   List<AuctionImage> findFirstImagesByAuctionIds(@Param("auctionIds") List<Long> auctionIds);
+
+  @Query("""
+      select
+            new com.windfall.api.user.dto.response.reviewlist.AuctionImageRaw(ai.auction.id, ai.image)
+      from AuctionImage ai
+      where ai.id in (
+        select min(ai2.id)
+        from AuctionImage ai2
+        where ai2.auction.id in :auctionIds
+        group by ai2.auction.id
+      )
+      """)
+  List<AuctionImageRaw> findFirstImagesProjection(@Param("auctionIds") List<Long> auctionIds);
 
   Optional<AuctionImage> findTop1ByAuctionIdOrderByIdAsc(Long auctionId);
 }
