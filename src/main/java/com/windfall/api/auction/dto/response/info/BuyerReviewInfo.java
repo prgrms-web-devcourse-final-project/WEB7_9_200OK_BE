ae68371fb24c3515047730d06e15cc4173af0c52
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-05 00:09:43 +0900

    refactor: Added descriptions some dtos

--- src/main/java/com/windfall/api/auction/dto/response/info/BuyerReviewInfo.java
+++ src/main/java/com/windfall/api/auction/dto/response/info/BuyerReviewInfo.java
@@ -1,8 +1,15 @@
 package com.windfall.api.auction.dto.response.info;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record BuyerReviewInfo(
+    @Schema(description = "구매자 ID")
     Long buyerId,
-    String name,
+
+    @Schema(description = "구매자 이름")
+    String username,
+
+    @Schema(description = "구매자 리뷰 내용 (리뷰를 작성하지 않았을 경우 null)")
     String content
 ) {
 }
