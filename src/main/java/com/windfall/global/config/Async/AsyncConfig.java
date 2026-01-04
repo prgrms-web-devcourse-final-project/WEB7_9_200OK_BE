fcf65f3490b1b595c1c902bd6ca967c97916b2b6
dpwl0974 <dpwl0974@gmail.com>
2026-01-04 22:08:44 +0900

    feature: 스레드 풀 설정을 위한 AsyncConfig 구현

---
+++ src/main/java/com/windfall/global/config/Async/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.windfall.global.config.Async;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+  @Bean(name = "socketTaskExecutor")
+  public Executor socketTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(10);
+    executor.setMaxPoolSize(50); // 기본 및 대기열 초과 시 최대 스레드 수
+    executor.setQueueCapacity(100); // 대기열
+    executor.setThreadNamePrefix("SocketExecutor-");
+    executor.initialize();
+    return  executor;
+  }
+
+}
