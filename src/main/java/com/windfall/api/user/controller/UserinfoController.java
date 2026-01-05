2b7f6198705f4762c788dc88634f45c52f78958b
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-05 10:53:48 +0900

    feat: Added UpdateUserImage, UpdateUsername Controller with swagger docs

--- src/main/java/com/windfall/api/user/controller/UserinfoController.java
+++ src/main/java/com/windfall/api/user/controller/UserinfoController.java
@@ -1,25 +1,30 @@
 package com.windfall.api.user.controller;
 
-import com.windfall.api.mypage.dto.dashboard.BaseDashBoardDetails;
+import com.windfall.api.user.dto.request.UpdateUsernameRequest;
+import com.windfall.api.user.dto.response.UpdateUsernameResponse;
 import com.windfall.api.user.dto.response.UserInfoResponse;
+import com.windfall.api.user.dto.response.UpdateUserProfileImageResponse;
 import com.windfall.api.user.dto.response.reviewlist.ReviewListResponse;
 import com.windfall.api.user.dto.response.saleshistory.BaseSalesHistoryResponse;
 import com.windfall.api.user.service.UserInfoService;
-import com.windfall.domain.auction.enums.AuctionStatus;
 import com.windfall.domain.user.entity.CustomUserDetails;
 import com.windfall.global.response.ApiResponse;
 import com.windfall.global.response.SliceResponse;
-import java.time.LocalDate;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -67,4 +72,29 @@ public class UserinfoController implements UserInfoSpecification{
     return ApiResponse.ok("사용자 리뷰 목록 조회에 성공하였습니다.", response);
   }
 
+  @Override
+  @PutMapping(path = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ApiResponse<UpdateUserProfileImageResponse> updateUserImage(
+      @RequestPart(name = "image") MultipartFile file,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ){
+    Long loginId = userDetails.getUserId();
+
+    UpdateUserProfileImageResponse response = userInfoService.updateUserProfileImage(file, loginId);
+
+    return ApiResponse.ok("사용자 프로필 이미지를 수정하였습니다.", response);
+  }
+
+  @Override
+  @PutMapping("/name")
+  public ApiResponse<UpdateUsernameResponse> updateUsername(
+      @RequestBody @Valid UpdateUsernameRequest request,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ){
+    Long loginId = userDetails.getUserId();
+
+    UpdateUsernameResponse response = userInfoService.updateUsername(request, loginId);
+
+    return ApiResponse.ok("사용자 이름을 수정하였습니다.", response);
+  }
 }
