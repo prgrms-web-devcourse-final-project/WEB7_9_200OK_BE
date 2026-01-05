dd54fdc085b6df629636ff328e94032511e8da3a
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-06 00:58:43 +0900

    feat: Added TradeController with swagger docs

--- src/main/java/com/windfall/api/trade/controller/TradeController.java
+++ src/main/java/com/windfall/api/trade/controller/TradeController.java
@@ -1,10 +1,33 @@
 package com.windfall.api.trade.controller;
 
+import com.windfall.api.trade.service.TradeService;
+import com.windfall.domain.user.entity.CustomUserDetails;
+import com.windfall.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TradeController {
+@RequestMapping("/api/v1/trade")
+public class TradeController implements TradeSpecification{
+
+  private final TradeService tradeService;
+
+  @Override
+  @PutMapping("/{tradeId}/confirm")
+  public ApiResponse<Void> purchaseConfirmTrade(
+      @PathVariable Long tradeId,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ){
+    Long userId = userDetails.getUserId();
+
+    tradeService.purchaseConfirmTrade(userId, tradeId);
+
+    return ApiResponse.ok("해당 경매상품의 구매를 확정하였습니다.", null);
+  }
 
 }
