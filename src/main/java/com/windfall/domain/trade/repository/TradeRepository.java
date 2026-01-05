42295544d440fd179d350997f50adc153579684a
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-06 01:00:34 +0900

    feat: Added Query in TradeRepository

--- src/main/java/com/windfall/domain/trade/repository/TradeRepository.java
+++ src/main/java/com/windfall/domain/trade/repository/TradeRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
   Optional<Trade> findByAuction(Auction auction);
+
+  Optional<Trade> findById(Long id);
 }
