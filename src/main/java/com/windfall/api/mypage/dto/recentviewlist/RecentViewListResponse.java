b8f6d56fc78b5654d37dbf290fc418ffc51aaabb
DEV-Cheeze <ckdwnd9999@gmail.com>
2026-01-06 10:30:56 +0900

    feat: addtional recentview dto fields: providing recentviewId

--- src/main/java/com/windfall/api/mypage/dto/recentviewlist/RecentViewListResponse.java
+++ src/main/java/com/windfall/api/mypage/dto/recentviewlist/RecentViewListResponse.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 
 @Getter
 @JsonPropertyOrder({
+    "recentViewId",
     "status",
     "auctionId",
     "title",
@@ -19,13 +20,14 @@ import lombok.Getter;
 public class RecentViewListResponse extends BaseRecentViewList{
 
   @Builder
-  public RecentViewListResponse(String status, Long auctionId, String title, String auctionImageUrl,
-      int startPrice, LocalDate startedAt) {
-    super(status, auctionId, title, auctionImageUrl, startPrice, startedAt);
+  public RecentViewListResponse(Long recentViewId, String status, Long auctionId, String title,
+      String auctionImageUrl, int startPrice, LocalDate startedAt) {
+    super(recentViewId, status, auctionId, title, auctionImageUrl, startPrice, startedAt);
   }
 
   public static RecentViewListResponse from(Tuple tuple){
     return RecentViewListResponse.builder()
+        .recentViewId(tuple.get("recentViewId", Long.class))
         .status(tuple.get("status", String.class))
         .auctionId(tuple.get("auctionId", Long.class))
         .title(tuple.get("title", String.class))
