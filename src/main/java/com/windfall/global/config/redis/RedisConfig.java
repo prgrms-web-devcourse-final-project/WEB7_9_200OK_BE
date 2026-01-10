da5f3df969db9517c0ec527ce46f07e179c344f9
dpwl0974 <dpwl0974@gmail.com>
2026-01-10 16:35:58 +0900

    refactor: 빈 충돌을 대비한 우선 순위 설정

--- src/main/java/com/windfall/global/config/redis/RedisConfig.java
+++ src/main/java/com/windfall/global/config/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import com.windfall.global.redis.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -28,6 +29,7 @@ public class RedisConfig {
   }
 
   @Bean
+  @Primary
   public RedisTemplate<String, String> redisTemplate() {
     RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 
