f276ac80fac5bd6736cf408afcb6f13706188172
dpwl0974 <dpwl0974@gmail.com>
2026-01-10 16:34:44 +0900

    refactor: redis pub/sub 구현체로 변경

--- src/main/java/com/windfall/api/auction/service/component/StompAuctionMessageSender.java
+++ src/main/java/com/windfall/api/auction/service/component/StompAuctionMessageSender.java
@@ -5,6 +5,7 @@ import com.windfall.api.auction.dto.response.message.AuctionViewerMessage;
 import com.windfall.api.auction.dto.response.message.SellerEmojiMessage;
 import com.windfall.domain.auction.enums.AuctionStatus;
 import com.windfall.domain.auction.enums.EmojiType;
+import com.windfall.global.redis.RedisPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -15,24 +16,24 @@ import org.springframework.stereotype.Component;
 @Async("socketTaskExecutor")
 public class StompAuctionMessageSender implements AuctionMessageSender {
 
-  private final SimpMessagingTemplate messagingTemplate;
+  private final RedisPublisher redisPublisher;
 
   @Override
   public void broadcastPriceUpdate(Long auctionId, Long currentPrice, AuctionStatus status) {
     AuctionMessage message = AuctionMessage.of(auctionId, currentPrice, status);
-    messagingTemplate.convertAndSend("/topic/auction/" + auctionId, message);
+    redisPublisher.publish("/topic/auction/" + auctionId, message);
   }
 
   @Override
   public void broadcastViewerCount(Long auctionId, long viewerCount) {
     AuctionViewerMessage message = AuctionViewerMessage.of(auctionId, viewerCount);
-    messagingTemplate.convertAndSend("/topic/auction/" + auctionId, message);
+    redisPublisher.publish("/topic/auction/" + auctionId, message);
 
   }
 
   @Override
   public void broadcastSellerEmoji(Long auctionId, EmojiType emojiType) {
     SellerEmojiMessage message = SellerEmojiMessage.of(auctionId, emojiType);
-    messagingTemplate.convertAndSend("/topic/auction/" + auctionId, message);
+    redisPublisher.publish("/topic/auction/" + auctionId, message);
   }
 }
