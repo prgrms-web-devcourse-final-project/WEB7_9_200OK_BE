7c7b10d17257a79caa569789dd8acf11de3d6c74
dpwl0974 <dpwl0974@gmail.com>
2026-01-10 16:34:16 +0900

    refactor: pub/sub을 위한 빈 설정 및 연결 설정 추가

---
+++ src/main/java/com/windfall/global/config/redis/RedisConfig.java
@@ -0,0 +1,81 @@
+package com.windfall.global.config.redis;
+
+import com.windfall.global.redis.RedisSubscriber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")
+  private String host;
+
+  @Value("${spring.data.redis.port}")
+  private int port;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(host, port);
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+
+  // Redis Pub/Sub을 위한 RedisTemplate 빈 설정
+  @Bean
+  public RedisTemplate<String, Object> pubSubRedisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+    // 토픽(channel) 이름: 문자열
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+    // 메시지 내용: JSON 객체 (직렬화)
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+    return redisTemplate;
+  }
+
+  @Bean
+  public ChannelTopic channelTopic() {
+    return new ChannelTopic("auction-topic");
+  }
+
+  @Bean
+  public RedisMessageListenerContainer redisMessageListenerContainer(
+      RedisConnectionFactory connectionFactory,
+      MessageListenerAdapter listenerAdapter,
+      ChannelTopic channelTopic
+  ) {
+    RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.addMessageListener(listenerAdapter, channelTopic);
+    return container;
+  }
+
+  @Bean
+  public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+    return new MessageListenerAdapter(subscriber, "onMessage");
+  }
+}
